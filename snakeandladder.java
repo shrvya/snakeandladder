6db0992389fbbcfa6a1a21ce412f9d6f6aa011f4
shrvya <shrivyashetty@gmail.com>
2021-08-20 20:25:27 +0530

    Branch Added

--- snakeandladder.java
+++ snakeandladder.java
@@ -1,5 +1,6 @@
 package snakeandladder;
 
+
 public class snakeandladder {
 
 	public static void main(String[] args) {
@@ -7,7 +8,9 @@ public class snakeandladder {
 		//This is use case1
 		int position=0;
 		System.out.println("Welcome to snake and Ladder");
-
+		//usecase2
+        int dicevalue=(int)( Math.random() * (6)) + 1; 
+        System.out.println("dice value is "+dicevalue);
 	}
 
 }
