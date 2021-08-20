c4f5eb333773beb907b431456d2c66a1225afe81
shrvya <shrivyashetty@gmail.com>
2021-08-20 20:14:18 +0530

    Branch Added

--- snakeandladder.java
+++ snakeandladder.java
@@ -4,6 +4,7 @@ public class snakeandladder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//This is use case1
 		int position=0;
 		System.out.println("Welcome to snake and Ladder");
 
