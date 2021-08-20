82d08240027c5618ff2b25c3140fb2392225b797
shrvya <shrivyashetty@gmail.com>
2021-08-20 20:54:17 +0530

    Branch Added

--- snakeandladder.java
+++ snakeandladder.java
@@ -7,10 +7,36 @@ public class snakeandladder {
 		// TODO Auto-generated method stub
 		//This is use case1
 		int position=0;
-		System.out.println("Welcome to snake and Ladder");
+		System.out.println("Welcome to snake and Ladder!!!");
+		
 		//usecase2
         int dicevalue=(int)( Math.random() * (6)) + 1; 
+       
         System.out.println("dice value is "+dicevalue);
+        //usecas3
+        int option=(int)( Math.random() * (3)) + 1; 
+        if(option==1)
+        {
+        	System.out.println("No Play!!");
+        }
+        else if(option==2)
+        {
+        	position+=dicevalue;
+        	System.out.println("Ladder!!");
+        }
+        else {
+        	position-=dicevalue;
+        	if(position<0)
+        	{
+        		position=0;
+        	}
+        	System.out.println("snake!!");
+        }
+        
+       
+        System.out.println("current postion is  "+position);
+    
 	}
+	
 
 }
