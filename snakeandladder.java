46e5ddb370107e0343adb0d35f030e2f214bf9b4
shrvya <shrivyashetty@gmail.com>
2021-08-20 20:09:30 +0530

    Added

---
+++ snakeandladder.java
@@ -0,0 +1,12 @@
+package snakeandladder;
+
+public class snakeandladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int position=0;
+		System.out.println("Welcome to snake and Ladder");
+
+	}
+
+}
